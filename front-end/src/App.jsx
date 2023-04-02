d6b815735cb5bc552fe5371752382a087f485155
DAN3002 <dinhanh300229@gmail.com>
2023-04-02 20:26:33 +0700

    Switch view

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,12 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-// import { useState } from 'react';
+import { useState } from 'react';
+
+import TransferTokenModal from './components/TransferTokenModal';
+import SwapTokenModal from './components/SwapTokenModal';
+import ImportAccount from './components/ImportAccount';
 
 function App() {
-	// const [activeTab, setActiveTab] = useState('swap');
+	const [isSwapActive, setIsSwapActive] = useState(true);
 
 	return (
 		<div className="container">
@@ -10,16 +14,30 @@ function App() {
 			<div className="main-content">
 				<div className="tab-container">
 					<div className="tab">
+						{/* <div className="tab__item tab__item--active" data-content-id="swap">SWAP</div>
+						<div className="tab__item" data-content-id="transfer">TRANSFER</div> */}
+
 						<div
-							className="tab__item tab__item--active"
+							className={`tab__item ${
+								isSwapActive ? 'tab__item--active' : ''
+							}`}
+							onClick={() => setIsSwapActive(true)}
 						>
 							SWAP
 						</div>
-						<div className="tab__item">
+						<div
+							className={`tab__item ${
+								!isSwapActive ? 'tab__item--active' : ''
+							}`}
+							onClick={() => setIsSwapActive(false)}
+						>
 							TRANSFER
 						</div>
 					</div>
 				</div>
+
+				{isSwapActive ? <SwapTokenModal /> : <TransferTokenModal />}
+				<ImportAccount />
 			</div>
 		</div>
 	);
