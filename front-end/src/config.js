c107cc8aa012e8200b77314f05e825d2cffd4ee4
DAN3002 <dinhanh300229@gmail.com>
2023-04-25 08:30:52 +0700

    Update new Exchange Contract

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -6,7 +6,7 @@ export const RPC_ENDPOINT = 'https://testnet.tomochain.com';
 export const EXCHANGE_ABI = ExchangeContract.abi;
 export const TOKEN_ABI = TokenContract.abi;
 export const BALANCE_REFRESH_INTERVAL = 10 * 1000;
-export const EXCHANGE_ADDRESS = '0xF9a2ba70C0C958D29f3fEcA49c45D97634FA5Af6';
+export const EXCHANGE_ADDRESS = '0x00711c309D38ae7Eeebe25326095fF86a8052689';
 
 // export const NATIVE_TOKEN = {
 // 	name: 'Ether',
@@ -24,11 +24,11 @@ export const TOKENS = [
 	{
 		name: 'Token A',
 		symbol: 'TKA',
-		address: '0x442EE946d3746e19DB87eE997d8866eCb3c4979d',
+		address: '0xe608239b6D00323A104cCcF7255c58e878599009',
 	},
 	{
 		name: 'Token B',
 		symbol: 'TKB',
-		address: '0xE895310d8Bc5A35173F45CE6200D93d46C72657d',
+		address: '0x201c6eFFb5cc71470086E9E223Fb4CA743974262',
 	},
 ];
